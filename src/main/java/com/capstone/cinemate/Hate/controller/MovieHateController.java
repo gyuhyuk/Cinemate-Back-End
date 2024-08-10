c3ffd378b5f6444e98ccc5b19f487c076e14df56
gyuhyuk <bruno3@naver.com>
2024-08-11 01:04:34 +0900

    feat : 싫어요 누른 영화 조회 API 구현

--- src/main/java/com/capstone/cinemate/Hate/controller/MovieHateController.java
+++ src/main/java/com/capstone/cinemate/Hate/controller/MovieHateController.java
@@ -1,7 +1,13 @@
 package com.capstone.cinemate.Hate.controller;
 
 import com.capstone.cinemate.Hate.service.MovieHateService;
+import com.capstone.cinemate.Member.controller.helper.TokenInformation;
+import com.capstone.cinemate.Movie.dto.MoviesResponse;
+import com.capstone.cinemate.common.response.CustomResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,4 +15,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class MovieHateController {
     private final MovieHateService movieHateService;
 
+    // 싫어요 누른 영화 조회
+    @GetMapping("/api/movie/hates")
+    public ResponseEntity<CustomResponse<MoviesResponse>> showDisLikeMovies(@TokenInformation Long memberId) {
+        MoviesResponse response = movieHateService.getDislikeMovies(memberId);
+        CustomResponse<MoviesResponse> customResponse = new CustomResponse<>(HttpStatus.OK.value(), "Success", response);
+        return ResponseEntity.ok().body(customResponse);
+    }
 }
