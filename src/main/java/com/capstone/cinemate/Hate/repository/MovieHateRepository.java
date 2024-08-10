74a25d93fba5948159d53845a86156119ff9291a
gyuhyuk <bruno3@naver.com>
2024-08-11 00:01:34 +0900

    feat : MovieHateRepository 기본 설정

---
+++ src/main/java/com/capstone/cinemate/Hate/repository/MovieHateRepository.java
@@ -0,0 +1,15 @@
+package com.capstone.cinemate.Hate.repository;
+
+import com.capstone.cinemate.Hate.domain.MovieHate;
+import com.capstone.cinemate.Member.domain.Member;
+import com.capstone.cinemate.Movie.domain.Movie;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MovieHateRepository extends JpaRepository<MovieHate, Long> {
+    Optional<MovieHate> findByMemberAndMovie(Member member, Movie movie);
+    List<MovieHate> findByMemberId(Long memberId);
+
+}
