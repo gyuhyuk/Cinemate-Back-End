d78fdd1010bbcd18f4c9ac43fea30fb72aaa94f0
gyuhyuk <bruno3@naver.com>
2024-08-22 23:18:36 +0900

    refactor : ErrorCode에 따른 HTTP Status 값 return 하도록 설정

--- src/main/java/com/capstone/cinemate/common/exception/ExceptionController.java
+++ src/main/java/com/capstone/cinemate/common/exception/ExceptionController.java
@@ -22,12 +22,13 @@ public class ExceptionController {
     }
 
     @ExceptionHandler(CustomException.class)
-    public CustomResponse<String> handleBaseException(CustomException e) {
-        return new CustomResponse<>(
+    public ResponseEntity<CustomResponse<String>> handleBaseException(CustomException e) {
+        CustomResponse customResponse= new CustomResponse<>(
                 e.getErrorCode().getStatus(),
                 e.getErrorCode().getCode(),
                 e.getErrorCode().getCause()
         );
+        return ResponseEntity.status(resolveHttpStatus(e.getErrorCode())).body(customResponse);
     }
 
     @ExceptionHandler({
@@ -43,15 +44,29 @@ public class ExceptionController {
         );
     }
 
-    // 요청 dto가 유효성 검사에서 틀렸을 때 발생
+
+    // MethodArgumentNotValidException 예외를 처리하는 핸들러(요청 바디가 잘못된 경우, 요청 dto에서 잘못된 경우 ex - 이메일 형식)
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    protected CustomResponse<String> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
-        return new CustomResponse<>(
-                HttpStatus.BAD_REQUEST.value(),
-                HttpStatus.BAD_REQUEST.getReasonPhrase(),
-                e.getBindingResult().getFieldErrors().get(0).getDefaultMessage()
-        );
+    public ResponseEntity<CustomResponse<String>> springValidationExceptionHandler(
+            final MethodArgumentNotValidException e) {
+
+        int status = HttpStatus.BAD_REQUEST.value();
+        String message = HttpStatus.BAD_REQUEST.getReasonPhrase();
+        String data = e.getBindingResult().getFieldErrors().get(0).getDefaultMessage();
+
+        CustomResponse<String> customResponse = new CustomResponse<>(status, message, data);
+        return ResponseEntity.badRequest().body(customResponse);
     }
+
+    // 요청 dto가 유효성 검사에서 틀렸을 때 발생
+//    @ExceptionHandler(MethodArgumentNotValidException.class)
+//    protected CustomResponse<String> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+//        return new CustomResponse<>(
+//                HttpStatus.BAD_REQUEST.value(),
+//                HttpStatus.BAD_REQUEST.getReasonPhrase(),
+//                e.getBindingResult().getFieldErrors().get(0).getDefaultMessage()
+//        );
+//    }
 
     @ExceptionHandler(NotFoundException.class)
     public CustomResponse<String> handleNotFoundException(NotFoundException e) {
@@ -60,5 +75,9 @@ public class ExceptionController {
                 HttpStatus.NOT_FOUND.getReasonPhrase(),
                 e.getMessage()
         );
+    }
+
+    private HttpStatus resolveHttpStatus(ErrorCode errorCode){
+        return HttpStatus.resolve(errorCode.getStatus());
     }
 }
